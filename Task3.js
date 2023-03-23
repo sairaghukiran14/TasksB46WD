fd30f6ce3f960c2bcc012008ef2bc45afd2b872d
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-03-23 17:07:06 +0530

    secondpush

--- Task3.js
+++ Task3.js
@@ -1,30 +1,56 @@
-//Checking the Objects whether they have same properties or not
+// //Checking the Objects whether they have same properties or not
 
-let obj1 = { name1: "Person1", age: 5 };
-let obj2 = { age: 5, name1: "Person1" };
-if (obj1.name1 == obj2.name1 && obj1.age == obj2.age) {
-  console.log("Both the Objects have same properties");
-}
-//
+// let obj1 = { name1: "Person1", age: 5 };
+// let obj2 = { age: 5, name1: "Person1" };
+// if (obj1.name1 == obj2.name1 && obj1.age == obj2.age) {
+//   console.log("Both the Objects have same properties");
+// }
+// //
 
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
-let fetchres = fetch(`https://restcountries.com/v3.1/all`);
-fetchres
-  .then((res) => res.json())
-  .then((d) => {
-    console.log(d);
-  })
-  .catch((err) => console.log(err));
+// let fetchres = fetch(`https://restcountries.com/v3.1/all`);
+// fetchres
+//   .then((res) => res.json())
+//   .then((d) => {
+//     console.log(d);
+//   })
+//   .catch((err) => console.log(err));
 
-//europe region displays
-let fetchresult = fetch(`https://restcountries.com/v3.1/region/europe`);
-fetchres
-  .then((res) => res.json())
-  .then((d) => {
-    console.log(d);
-  })
-  .catch((err) => console.log(err));
+// //europe region displays
+// let fetchresult = fetch(`https://restcountries.com/v3.1/region/europe`);
+// fetchres
+//   .then((res) => res.json())
+//   .then((d) => {
+//     console.log(d);
+//   })
+//   .catch((err) => console.log(err));
 
-  //medium Questions
-  
+//medium Questions
+// Task 1 to to Variable problems
+let t1, t2, t3, t4;
+console.log(t1, t2, t3, t4);
+
+var tarr = "Hello, world";
+console.log(tarr);
+
+var firstname = "John";
+var lastname = "doe";
+var maritalstaus = "married";
+var country = "India";
+var age = 24;
+console.log(firstname);
+console.log(lastname);
+console.log(maritalstaus);
+console.log(country);
+console.log(age);
+
+let str = true;
+let fir;
+let nu = null;
+console.log(str, fir, nu);
+
+let cun = "1234";
+console.log(typeof parseInt(cun));
+console.log(typeof Number(cun));
+console.log(2 + "1");
