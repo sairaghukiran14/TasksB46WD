aa0c4fddfde3f2a9045a6b2c8610765aa0991e11
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-03-25 23:08:06 +0530

    ....as

--- Task5.js
+++ Task5.js
@@ -11,12 +11,12 @@ oddnum(arr);
 
 //Convert all the strings to title caps in a string array
 
-let arr1 = ["hello", "world", "rog", "fred"];
-for (let i = 0; i < arr1.length; i++) {
-  let t;
-  t = arr1[i].charAt(0).toUpperCase() + arr[i].slice(1);
-  arr[i] = t;
-}
+// let arr1 = ["hello", "world", "rog", "fred"];
+// for (let i = 0; i < arr1.length; i++) {
+//   let t;
+//   t = arr1[i].charAt(0).toUpperCase() + arr[i].slice(1);
+//   arr[i] = t;
+// }
 
 // sum of all the numbers in array
 let arr2 = [1, 5, 8, 2, 22, 9, 19, 33];
@@ -69,8 +69,21 @@ palindromeall(arr4);
 //Return median of two sorted arrays of the same size.
 let sarr1 = [1, 2, 3, 4, 5, 6, 7, 8];
 let sarr2 = [11, 13, 14];
-let medianof2 = () => {};
+let t1;
+let medianof2 = (sarr1, sarr2) => {
+  let sarr12 = [...sarr1, ...sarr2].sort(function (a, b) {
+    return a - b;
+  });
 
+  if (sarr12.length % 2 == 0) {
+    t1 = (sarr12[sarr12.length / 2] + sarr12[sarr12.length / 2 - 1]) / 2;
+  } else {
+    t1 = sarr12[Math.floor(sarr12.length / 2)];
+  }
+  return t1;
+};
+
+console.log(medianof2(sarr1, sarr2));
 //Remove duplicates from an array
 
 let a1 = [1, 3, 3, 4, 5, 5, 7, 8, 9];
